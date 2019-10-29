0bd24cb1a53fa041dfb0c83d390183365fc53108
Ardash <56517452+ardache@users.noreply.github.com>
2019-10-29 16:53:29 -0600

    avance
    x

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -6,9 +6,46 @@
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
+function orderAlphabetically(array) {
+    let newArray = array.map(ele => ele);
+
+    newArray.sort(function(a,b){
+        if (a.title > b.title) return 1;
+        else if (a.title < b.title) return -1;
+        else return 0;
+        })
+    let first20 = newArray.map(title => title.title).slice(0,20);
+    return first20;
+}
+
+console.log(orderAlphabetically(movies))
+
+
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
+function ratesAverage(array) {
+    let movieRates = movies.map(function (array) {
+         let rate = Number(array.rate)
+         return rate;
+     })
+    //console.log(movieRates)
+    
+    let reducedRate = movieRates.reduce(function (acu, ele) {
+         var sum = acu + ele;
+         var roundedSum = sum;
+         return roundedSum;
+     }, 0); 
+    
+    //console.log((reducedRate/array.length).toFixed(2));
+    return (reducedRate/array.length).toFixed(2)
+    }
+    
+    
+    console.log(ratesAverage(movies))
+
 // Iteration 5: Drama movies - Get the average of Drama Movies
+
+let average
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
